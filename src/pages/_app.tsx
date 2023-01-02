79013fe95ade5071f359c031b61fd6e80d464ee3
Markus Juopperi <markus@juopperi.io>
2023-01-02 18:28:05 +0200

    Move common head elements to proper places

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import type {AppProps} from 'next/app'
 import {Barlow} from '@next/font/google'
+import Head from 'next/head'
 
 const mainFont = Barlow({subsets: ['latin'], weight: ['200']})
 
 export default function App({Component, pageProps}: AppProps) {
   return (
-    <main className={mainFont.className}>
-      <Component
-        {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={mainFont.className}>
+        <Component
+          {...pageProps} />
+      </main>
+    </>
   )
 }
