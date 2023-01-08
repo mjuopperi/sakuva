c0da2b263d6d5e9de0ef5517503e967eea3961f5
Markus Juopperi <markus@juopperi.io>
2023-01-08 15:13:23 +0200

    Replace nextjs Image with img because Image is slow

--- src/components/ImageList/Image/Image.tsx
+++ src/components/ImageList/Image/Image.tsx
@@ -1,10 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
-import ImageComponent from 'next/image'
 
 import { imgUrl } from '../../../api/apiUtils'
 import { Image as ImageModel } from '../../../api/imageApi'
 
 import './Image.scss'
+
+
+const THUMBNAIL_SIZES = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200]
+
+function srcset(image: ImageModel): string {
+  const [basePath, extension] = image.urlPath.split('.')
+  const baseUrl = imgUrl(basePath)
+  const sources = THUMBNAIL_SIZES.map(size => `${baseUrl}_${size}.${extension} ${size}w`)
+  return sources.join(',')
+}
+
 
 export default function Image({ image }: {image: ImageModel}) {
   return (
@@ -12,17 +23,18 @@ export default function Image({ image }: {image: ImageModel}) {
       key={image.id}
       className={[
         'image',
+        'image--fill',
         `image--${image.width > image.height ? 'horizontal' : 'vertical'}`,
         `image--${image.isColor ? 'color' : 'grayscale'}`,
       ].join(' ')}
     >
-      <ImageComponent
+      <img
         src={imgUrl(image.urlPath)}
-        alt={image.caption}
-        fill
+        srcSet={srcset(image)}
         sizes="(max-width: 768px) 100vw,
                      (max-width: 1200px) 50vw,
                      33vw"
+        alt={image.caption}
       />
     </div>
   )
