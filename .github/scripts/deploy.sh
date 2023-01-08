c7a58425665a5e69e3d7980faae977688ae499ce
Markus Juopperi <markus@juopperi.io>
2023-01-08 21:31:42 +0200

    Deploy

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,13 @@
+# Setup ssh
+mkdir -p "$HOME"/.ssh
+ssh-keyscan "$SERVER_ADDRESS" >> "$HOME"/.ssh/known_hosts
+eval "$(ssh-agent)"
+ssh-add - <<< "$SSH_PRIVATE_KEY"
+
+
+# Pull and restart web container
+ssh "$SSH_USERNAME@$SERVER_ADDRESS" "
+  cd /data/sakuva
+  sudo docker-compose pull --quiet web
+  sudo docker-compose restart web
+"
