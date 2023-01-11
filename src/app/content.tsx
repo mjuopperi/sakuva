b205cd4b8d08ad74275333552d3e5e858194a3b5
Markus Juopperi <markus@juopperi.io>
2023-01-11 14:43:44 +0200

    Infinite scroll

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
+
 import { defaultQueryOptions, Image } from '../api/imageApi'
 import { useInfiniteSearchImages } from '../api/imageHooks'
 import ImageList from '../components/ImageList'
-import { useState } from 'react'
 import Search from '../components/Search/Search'
 import Button from '../components/common/Button'
 
@@ -15,6 +17,8 @@ interface ContentProps {
 }
 
 export default function Content({ initialData, initialTotal }: ContentProps) {
+  // rootMargin is top, right, bottom, left
+  const { ref: loadMoreButtonRef, inView: loadMoreButtonInView } = useInView({ rootMargin: '0px 0px 50% 0px' })
   const [query, setQuery] = useState(defaultQueryOptions)
   const {
     data,
@@ -26,12 +30,22 @@ export default function Content({ initialData, initialTotal }: ContentProps) {
   } = useInfiniteSearchImages(query, initialData)
   const images = data?.pages.flatMap(page => page.data) || []
 
+  useEffect(() => {
+    if (loadMoreButtonInView) fetchNextPage()
+    // We only want to run this when visibility of the button changes
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loadMoreButtonInView])
+
   return (
     <div className="content">
       <Search query={query} setQuery={setQuery}/>
       { hasPreviousPage && <Button onClick={() => fetchPreviousPage()} disabled={isFetching}>Lataa lisää</Button>}
       <ImageList images={images} />
-      { hasNextPage && <Button onClick={() => fetchNextPage()} disabled={isFetching}>Lataa lisää</Button>}
+      { hasNextPage && (
+        <div className="button-container" ref={loadMoreButtonRef}>
+          <Button onClick={() => fetchNextPage()} disabled={isFetching}>Lataa lisää</Button>
+        </div>
+      )}
     </div>
   )
 }
